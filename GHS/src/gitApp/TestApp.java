29e71d5b5d291f83ac4cf55446f466944fef1868
Kang <u8360182@gmail.com>
2022-06-03 09:58:33 +0800

    Add Math Branch

--- GHS/src/gitApp/TestApp.java
+++ GHS/src/gitApp/TestApp.java
@@ -3,8 +3,16 @@ package gitApp;
 public class TestApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("1st Version");
+		Math cMath = new Math();
+		System.out.println();
+		System.out.print(cMath.add(10, 5));
 	}
 
+}
+
+class Math {
+	int add(int a, int b) {
+		return a + b;
+	}
 }
